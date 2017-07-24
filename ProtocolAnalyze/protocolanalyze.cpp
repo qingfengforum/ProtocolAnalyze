4f7873894071fbdd56680da365f9fd12a2effdf4
qingfengforum <qingfeng_forum@163.com>
2017-07-24 20:26:52 +0800

    basic connected and disconnected
    - realise basic func connected and disconnected.

--- ProtocolAnalyze/protocolanalyze.cpp
+++ ProtocolAnalyze/protocolanalyze.cpp
@@ -2,8 +2,7 @@
 #include "ui_protocolanalyze.h"
 
 #include <QtSerialPort/QSerialPortInfo>
-#include <QIntValidator>
-#include <QLineEdit>
+#include <QMessageBox>
 
 QT_USE_NAMESPACE
 static const char blankString[] = QT_TRANSLATE_NOOP("SettingsDialog", "N/A");
@@ -13,8 +12,58 @@ ProtocolAnalyze::ProtocolAnalyze(QWidget *parent) :
     ui(new Ui::ProtocolAnalyze)
 {
     ui->setupUi(this);
+
+    serial = new QSerialPort(this);
+
+    status = new QLabel;
+    ui->statusBar->addWidget(status);
+
+    fillPortsParameters();
     fillPortsInfo();
+
+    initActionsConnections();
+
 }
+
+ProtocolAnalyze::~ProtocolAnalyze()
+{
+    delete ui;
+}
+
+void ProtocolAnalyze::fillPortsParameters()
+{
+    ui->baudRateBox->addItem(QStringLiteral("9600"), QSerialPort::Baud9600);
+    ui->baudRateBox->addItem(QStringLiteral("19200"), QSerialPort::Baud19200);
+    ui->baudRateBox->addItem(QStringLiteral("38400"), QSerialPort::Baud38400);
+    ui->baudRateBox->addItem(QStringLiteral("115200"), QSerialPort::Baud115200);
+    //ui->baudRateBox->addItem(tr("Custom"));
+
+    ui->dataBitsBox->addItem(QStringLiteral("5"), QSerialPort::Data5);
+    ui->dataBitsBox->addItem(QStringLiteral("6"), QSerialPort::Data6);
+    ui->dataBitsBox->addItem(QStringLiteral("7"), QSerialPort::Data7);
+    ui->dataBitsBox->addItem(QStringLiteral("8"), QSerialPort::Data8);
+    ui->dataBitsBox->setCurrentIndex(3);
+
+    ui->parityBox->addItem(tr("None"), QSerialPort::NoParity);
+    ui->parityBox->addItem(tr("Even"), QSerialPort::EvenParity);
+    ui->parityBox->addItem(tr("Odd"), QSerialPort::OddParity);
+    ui->parityBox->addItem(tr("Mark"), QSerialPort::MarkParity);
+    ui->parityBox->addItem(tr("Space"), QSerialPort::SpaceParity);
+
+    ui->stopBitsBox->addItem(QStringLiteral("1"), QSerialPort::OneStop);
+#ifdef Q_OS_WIN
+    ui->stopBitsBox->addItem(tr("1.5"), QSerialPort::OneAndHalfStop);
+#endif
+    ui->stopBitsBox->addItem(QStringLiteral("2"), QSerialPort::TwoStop);
+
+    ui->flowControlBox->addItem(tr("None"), QSerialPort::NoFlowControl);
+    ui->flowControlBox->addItem(tr("RTS/CTS"), QSerialPort::HardwareControl);
+    ui->flowControlBox->addItem(tr("XON/XOFF"), QSerialPort::SoftwareControl);
+}
+
+/*
+ * @ brief : auto fill the available port name
+ */
 void ProtocolAnalyze::fillPortsInfo()
 {
     ui->serialPortInfoListBox->clear();
@@ -39,7 +88,70 @@ void ProtocolAnalyze::fillPortsInfo()
     }
 
 }
-ProtocolAnalyze::~ProtocolAnalyze()
+/*
+ * @ brief : open a serial port
+ */
+void ProtocolAnalyze::openSerialPort()
 {
-    delete ui;
+    serial->setPortName(ui->serialPortInfoListBox->currentText());
+    serial->setBaudRate(ui->baudRateBox->currentText().toInt());
+    serial->setDataBits(static_cast<QSerialPort::DataBits>(
+                            ui->dataBitsBox->itemData(ui->dataBitsBox->currentIndex()).toInt()));
+    serial->setParity(static_cast<QSerialPort::Parity>(
+                          ui->parityBox->itemData(ui->parityBox->currentIndex()).toInt()));
+    serial->setStopBits(static_cast<QSerialPort::StopBits>(
+                            ui->stopBitsBox->itemData(ui->stopBitsBox->currentIndex()).toInt()));
+    serial->setFlowControl(static_cast<QSerialPort::FlowControl>(
+                               ui->flowControlBox->itemData(ui->flowControlBox->currentIndex()).toInt()));
+    if (serial->open(QIODevice::ReadWrite)) {
+        ui->actionConnect->setEnabled(false);
+        ui->actionDisconnect->setEnabled(true);
+        showStatusMessage(tr("Connected Success!"));
+    } else {
+        QMessageBox::critical(this, tr("Error"), serial->errorString());
+
+        showStatusMessage(tr("Open error"));
+    }
+
+}
+
+void ProtocolAnalyze::closeSerialPort()
+{
+    if (serial->isOpen())
+        serial->close();
+    ui->actionConnect->setEnabled(true);
+    ui->actionDisconnect->setEnabled(false);
+    showStatusMessage(tr("Disconnected"));
+}
+
+/*
+ *@ brief : initial actions(signals) with slot.
+ */
+void ProtocolAnalyze::initActionsConnections()
+{
+    connect(ui->actionConnect, &QPushButton::clicked, this, &ProtocolAnalyze::openSerialPort);
+    connect(ui->actionDisconnect, &QPushButton::clicked, this, &ProtocolAnalyze::closeSerialPort);
+}
+
+/***************************************************/
+/************* actions *****************************/
+/***************************************************/
+/*
+ * @ brief : [refresh port] refresh available port name information
+ */
+void ProtocolAnalyze::on_pB_refreshPortInfo_clicked()
+{
+    ui->serialPortInfoListBox->clear();
+    const auto infos = QSerialPortInfo::availablePorts();
+    for (const QSerialPortInfo &info : infos) {
+        QStringList list;
+        list << info.portName();
+
+        ui->serialPortInfoListBox->addItem(list.first(), list);
+    }
+}
+
+void ProtocolAnalyze::showStatusMessage(const QString &message)
+{
+    status->setText(message);
 }
--- ProtocolAnalyze/protocolanalyze.h
+++ ProtocolAnalyze/protocolanalyze.h
@@ -2,6 +2,10 @@
 #define PROTOCOLANALYZE_H
 
 #include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+
+#include <QMessageBox>
+#include <QLabel>
 
 namespace Ui {
 class ProtocolAnalyze;
@@ -12,12 +16,43 @@ class ProtocolAnalyze : public QMainWindow
     Q_OBJECT
 
 public:
+    struct Settings {
+        QString name;
+        qint32 baudRate;
+        QString stringBaudRate;
+        QSerialPort::DataBits dataBits;
+        QString stringDataBits;
+        QSerialPort::Parity parity;
+        QString stringParity;
+        QSerialPort::StopBits stopBits;
+        QString stringStopBits;
+        QSerialPort::FlowControl flowControl;
+        QString stringFlowControl;
+        bool localEchoEnabled;
+    };
+
+public:
     explicit ProtocolAnalyze(QWidget *parent = 0);
     ~ProtocolAnalyze();
 
+private slots:
+    void openSerialPort();
+    void closeSerialPort();
+
+    void on_pB_refreshPortInfo_clicked();
+
+private:
+    void fillPortsInfo();
+    void initActionsConnections();
+    void fillPortsParameters();
+    void showStatusMessage(const QString &message);
+
 private:
     Ui::ProtocolAnalyze *ui;
-    void fillPortsInfo();
+    Settings portSetting;
+    QLabel *status;
+    QSerialPort *serial;
+
 };
 
 #endif // PROTOCOLANALYZE_H
